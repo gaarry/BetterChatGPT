d4815256ff1658a94dcb5dbd1fbf3898a6fd25a3
gary <18203990104@163.com>
2023-10-09 17:56:12 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,8 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+export const officialAPIEndpoint = 'https://gateway.ai.cloudflare.com/v1/c78a0cc169ce862c120e873509f4150c/aigateway/openai/chat/completions';
+export const officialAPIEndpoin1 = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
-export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
+export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint, officialAPIEndpoin1];

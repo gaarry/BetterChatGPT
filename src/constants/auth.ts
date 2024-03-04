85e4cd17e3004d56a6d082d0111c4eca78199ee0
gary <garyyao18@gmail.com>
2024-03-04 10:55:45 +0800

    update auth menu

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,8 +1,9 @@
 export const officialAPIEndpoint = 'https://gateway.ai.cloudflare.com/v1/c78a0cc169ce862c120e873509f4150c/aigateway/openai/chat/completions';
 export const officialAPIEndpoin1 = 'https://api.openai.com/v1/chat/completions';
+export const ehco = 'https://openai.ehco-relay.cc';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
-export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint, officialAPIEndpoin1];
+export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint, officialAPIEndpoin1, ehco];
